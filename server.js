b036321009ec4129be540a28048901c541666195
Asad Meer <meerasads636@gmail.com>
2024-03-02 01:31:24 +0500

    cluster testing

--- server.js
+++ server.js
@@ -83,3 +83,12 @@ if (cluster.isPrimary) {
     console.log(`Worker ${cluster.worker.id} listening on port ${port}`);
   });
 }
+
+
+// const http = require ("http");
+// const app = require("./app");
+// const port = process.env.PORT || 3000;
+// const server = http.createServer(app);
+// server.listen(port);
+
+
